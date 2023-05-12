3d0a550bf79c7f83fc2d2e1b0edac69d4324df78
You-Jen Chang <tstren1223@gmail.com>
2023-05-12 13:41:28 +0200

    d

--- src/test/java/de/tum/in/ase/eist/PersonServiceTest.java
+++ src/test/java/de/tum/in/ase/eist/PersonServiceTest.java
@@ -76,27 +76,17 @@ class PersonServiceTest {
     @Test
     void testAddThreeParents() {
         var person = new Person();
-        person.setFirstName("M");
-        person.setLastName("A");
-        person.setBirthday(LocalDate.now());
-
-        personService.save(person);
         var person2 = new Person();
-        person2.setFirstName("M");
-        person2.setLastName("B");
-        person2.setBirthday(LocalDate.now());
-        personService.save(person2);
         var person3 = new Person();
-        person3.setFirstName("M");
-        person3.setLastName("C");
-        person3.setBirthday(LocalDate.now());
-
-        personService.save(person3);
         var person4 = new Person();
-        person4.setFirstName("M");
-        person4.setLastName("D");
+        person.setBirthday(LocalDate.now());
+        person2.setBirthday(LocalDate.now());
+        person3.setBirthday(LocalDate.now());
         person4.setBirthday(LocalDate.now());
+        personService.save(person);
+        personService.save(person2);
         personService.save(person4);
+        personService.save(parent3);
         personService.addParent(person,person2);
         personService.addParent(person,person3);
         assertThrows(ResponseStatusException.class,()->{
